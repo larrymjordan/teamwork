1eeb80df26f2e1b4ed4a628c8ddb347a795126ca
Larry Morales <larrymoralesjordan@gmail.com>
2014-10-29 20:38:41 -0500

    i18n for edit members content

--- app/views/members/edit.blade.php
+++ app/views/members/edit.blade.php
@@ -2,10 +2,8 @@
 @section('content')
 <div class="row">
  <div class="columns large-6 large-centered create-container">
-  <h1 class="form-title">Actualiza los datos de tu miembro</h1>
-  <p class="information">
-    Cambia el dato que quieras de tu miembro en la casilla correspondiente. Los cambios se veran reflejados automaticamente.
-  </p>
+  <h1 class="form-title">{{Lang::get('general.edit_member_title')}}</h1>
+  <p class="information">{{Lang::get('general.edit_member_information')}}</p>
   {{ Form::model($member, array('route' => ['members.update', $member->id], 'method' => 'PUT') ) }}
 
     {{ Form::label('name', 'Nombre', array('class'=>'label-left')) }}
